25b5aed8453faf061148b9564e3bc4c0a17f76e6
JuCJeff <zma67@wisc.edu>
2019-07-05 15:36:10 -0500

    Added vuex and vuetify dependency

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
+import Vuex from 'vuex'
+import Vuetify from 'vuetify'
 import App from './App.vue'
 
 Vue.config.productionTip = false
+Vue.use(Vuex)
+Vue.use(Vuetify)
 
 new Vue({
   render: h => h(App),
