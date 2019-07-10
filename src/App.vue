e86e3ba3ac9404ef9b36009f3a39d11cda29c7b8
JuCJeff <zma67@wisc.edu>
2019-07-09 22:32:17 -0500

    Added dark mode switch to the boilerplate

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,50 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Boilerplate for Cell Netowrk Viewer"/>
+    <v-app :dark="goDark">
+      <v-container bg grid-list-md text-xs-center>
+        <v-layout row justify-space-between>
+          <v-flex md2>
+            <v-switch color="primary" :label="selectedTheme" v-model="goDark"></v-switch>
+          </v-flex>
+          <v-spacer></v-spacer>
+        </v-layout>
+        <v-layout row wrap align-center>
+          <v-flex>
+            <HelloWorld msg="Boilerplate for Cell Netowrk Viewer" />
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-app>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
     HelloWorld
+  },
+  computed: {
+    selectedTheme: function() {
+      return this.goDark ? "Dark Theme" : "Light Theme";
+    }
+  },
+  data: function() {
+    return {
+      goDark: false,
+      search: ""
+    };
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,13 +1,21 @@
 import Vue from 'vue'
-import Vuex from 'vuex'
-import Vuetify from 'vuetify'
 import App from './App.vue'
+import Vuetify from 'vuetify'
+import Vuex from 'vuex'
 
 import 'vuetify/dist/vuetify.min.css'
 
 Vue.config.productionTip = false
 Vue.use(Vuex)
-Vue.use(Vuetify)
+Vue.use(Vuetify, {
+  iconfont: 'md',
+  theme: {
+    primary: '#42b983',
+    secondary: '#b0bec5',
+    accent: '#8c9eff',
+    error: '#b71c1c'
+  }
+});
 
 new Vue({
   render: h => h(App),
