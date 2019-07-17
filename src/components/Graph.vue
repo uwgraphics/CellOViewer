9befecaf4eb478bf825d744fb74f7ead80fd9bc1
JuCJeff <zma67@wisc.edu>
2019-07-17 12:19:49 -0500

    Added rough view of D3-DAG graph

--- src/components/Graph.vue
+++ src/components/Graph.vue
@@ -25,8 +25,9 @@
 </template>
 
 <script>
-// import * as d3 from "d3";
-// import * as d3Dag from "d3-dag";
+import { formatWithOptions } from "util";
+import * as d3 from "d3";
+import * as d3Dag from "d3-dag";
 
 export default {
   name: "cell-graph",
@@ -39,19 +40,83 @@ export default {
   },
   methods: {
     showDag() {
-      // let width = 500;
-      // let height = 500;
-      // let formatData = d3Dag.dagStratify(this.cellData);
-      // let dag = d3Dag.dagHierarchy(formatData);
+      let width = 600;
+      let height = 600;
+      // Change our dataset to a node links structure(for dag structure conversion)
+      let links = [];
+      Object.keys(this.cellData).forEach(node => {
+        let children = this.cellData[node];
+        children.forEach(child => {
+          links.push([node, child]);
+        });
+      });
 
-      // let svg = d3
-      //   .select(this.$refs.graph)
-      //   .append(svg)
-      //   .attr("preserveAspectRatio", "xMinYMin meet")
-      //   .attr("viewBox", `0 0 ${width} ${height}`);
-      // svg.append(d3Dag.layeringLongestPath(dag));
-      // console.log(dag);
-      // Continue to load the svg graph into the graph component
+      let transformedData = d3Dag.dagConnect()(links);
+      let layout = d3Dag.sugiyama().coord(d3Dag.coordCenter())(transformedData);
+      console.log("LINKS", layout.links());
+      console.log("NODES", layout.descendants());
+      let svg = d3
+        .select(this.$refs.graph)
+        .append("svg")
+        .attr("preserveAspectRatio", "xMinYMin meet")
+        .attr("viewBox", `0 0 ${width} ${height}`);
+
+      const line = d3
+        .line()
+        .curve(d3.curveCatmullRom)
+        .x(d => d.x * width)
+        .y(d => d.y * height);
+
+      const g = svg.append("g").attr("transform", `translate(${50},${50})`);
+
+      g.append("g")
+        .selectAll("path")
+        .data(layout.links())
+        .enter()
+        .append("path")
+        .attr("d", ({ source, target, data }) =>
+          line(
+            [
+              {
+                x: source.x,
+                y: source.y
+              }
+            ].concat(data.points || [], [
+              {
+                x: target.x,
+                y: target.y
+              }
+            ])
+          )
+        )
+        .attr("fill", "none")
+        .attr("stroke", "#42b983");
+
+      const nodes = g
+        .append("g")
+        .selectAll("g")
+        .data(layout.descendants())
+        .enter()
+        .append("g")
+        .attr(
+          "transform",
+          ({ x, y }) => `translate(${x * width}, ${y * height})`
+        );
+
+      nodes
+        .append("circle")
+        .attr("r", 5)
+        .attr("fill", "white")
+        .attr("stroke", "black");
+
+      // Add text, which screws up measureement
+      nodes
+        .append("text")
+        .text(d => d.id)
+        .attr("text-anchor", "middle")
+        .attr("alignment-baseline", "middle")
+        .style("font-size", "10px");
+      // yield svgNode;
     }
   },
   computed: {
